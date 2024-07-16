199c2eec4aaf948df52abb525f598289e8779dea
Ondra <andrewskrobak@ondra.lan>
2024-07-16 09:06:58 -0700

    ribbons

--- cellInit.js
+++ cellInit.js
@@ -274,7 +274,7 @@ export function initCellRenderer() {
 
   const loadPromises = [
     new CellComponent("blob-outer.gltf", null, 2),
-    new CellComponent("ribbons-henry_v1.glb", grayPurple, 3),
+    new CellComponent("ribbons_reduced.glb", grayPurple, 3),
     new CellComponent("blob-inner.glb", iridescent, 1)
   ];
 
