f11696ba6cf54bc2eddeb892b7a8eebf9a8a855b
Ondra <andrewskrobak@ondra.lan>
2024-08-08 09:18:37 -0700

    normalized

--- js/cellInit.js
+++ js/cellInit.js
@@ -250,10 +250,9 @@ export function initCellRenderer() {
     });
 
     const loadPromises = [
-      //new CellComponent("blob-outer.gltf", null, 2),
-      new CellComponent("blob-outer_compressed.gltf", null, 2),
+      new CellComponent("blob-outer_compressed.gltf", grayPurple, 2),
       new CellComponent("ribbons.glb", grayPurple, 3),
-      new CellComponent("blob-inner.glb", iridescent, 1)
+      // new CellComponent("blob-inner.glb", iridescent, 1)
     ];
 
     let waveShader;
