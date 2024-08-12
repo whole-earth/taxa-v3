1b95410df313a8602a47b6b9b8f05dbd8cdb2215
Ondra <andrewskrobak@ondra.lan>
2024-08-12 15:40:42 -0700

    new blob

--- js/cellInit.js
+++ js/cellInit.js
@@ -235,7 +235,7 @@ export function initCellRenderer() {
     });
 
     const loadPromises = [
-      new CellComponent("blob-outer.gltf", null, 2),
+      new CellComponent("blob-outer_new.gltf", null, 2),
       new CellComponent("ribbons.glb", grayPurple, 3),
       new CellComponent("blob-inner.glb", iridescent, 1)
     ];
