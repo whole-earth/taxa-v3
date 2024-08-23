184ea5094da27b06abab0a4dda4eb0caefb0ff76
Ondra <andrewskrobak@ondra.lan>
2024-08-23 11:36:20 -0700

    dots

--- js/scroll.js
+++ js/scroll.js
@@ -46,8 +46,6 @@ function scrollLogic(controls, camera, spheres, wavingBlob, dotBounds, product) 
         zoomProgress = scrollProgress(zoomArea);
         camera.fov = smoothLerp(zoomStartFOV, zoomEndFOV, zoomProgress);
 
-        //console.log(wavingBlob.parent.scale)
-
         if (!zoomAlready) {
             activateText(zoomArea);
             splashAlready = false;
@@ -295,10 +293,62 @@ function dotTweenOpacity(spheres, initialOpacity, targetOpacity, wavingBlob, sca
         const currentState = { opacity: initialOpacity };
         const targetState = { opacity: targetOpacity };
 
+        const sphereTween = new Tween(currentState)
+            .to(targetState, duration)
+            .easing(Easing.Quadratic.InOut)
+            .onUpdate(() => {
+                sphere.material.opacity = currentState.opacity;
+                sphere.material.needsUpdate = true;
+            })
+            .onComplete(() => {
+                tweenGroup.remove(tween);
+            });
+        
+        tweenGroup.add(sphereTween);
+        tween.start();
+    });
+
+    if (scale) {
+        const initialScale = { scale: 0.7 };
+        const targetScale = { scale: 1.0 };
+
+        const scaleTween = new Tween(initialScale)
+            .to(targetScale, (duration*1.5))
+            .easing(Easing.Quadratic.InOut)
+            .onUpdate(() => {
+                wavingBlob.scale.set(initialScale.scale, initialScale.scale, initialScale.scale);
+                //wavingBlob.parent.scale.set(initialScale.scale, initialScale.scale, initialScale.scale);
+            })
+            .onComplete(() => {
+                tweenGroup.remove(scaleTween);
+            });
+
+        tweenGroup.add(scaleTween);
+        scaleTween.start();
+    }
+}
+
+function dotTweenOpacityEx(spheres, initialOpacity, targetOpacity, wavingBlob, scale = false, duration = 300) {
+    // Reset array each time it's called
+    tweenGroup.removeAll();
+
+    spheres.forEach(sphere => {
+        const currentState = { 
+            opacity: initialOpacity,
+            scale: scale ? 0.7 : sphere.scale.x
+        };
+        const targetState = { 
+            opacity: targetOpacity,
+            scale: scale ? 1.0 : sphere.scale.x
+        };
+
         const tween = new Tween(currentState)
             .to(targetState, duration)
             .easing(Easing.Quadratic.InOut)
             .onUpdate(() => {
+                if (scale) {
+                    sphere.scale.set(currentState.scale, currentState.scale, currentState.scale);
+                }
                 sphere.material.opacity = currentState.opacity;
                 sphere.material.needsUpdate = true;
             })
