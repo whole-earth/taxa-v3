5965d4b30f87a5b55d3a527b7440c44395416a65
Ondra <andrewskrobak@ondra.lan>
2024-09-02 20:22:46 -0700

    add test

--- js/scroll.js
+++ js/scroll.js
@@ -157,6 +157,7 @@ function scrollLogic(controls, camera, cellObject, blobInner, ribbons, spheres, 
             });
 
             if (comingFrom == 'zoomAreaThird') {
+                console.log("YES")
                 dotTweenOpacity(spheres, 1, 0, wavingBlob, fadeOutDuration);
                 ribbonTweenOpacity(ribbons, 0, 1);
                 cellSheenTween(blobInner);
